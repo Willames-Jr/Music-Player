05d34894af67ef0c1cda86ffe603cb63ef0a3487
unknown <willamesj11@gmai.com>
2020-06-16 21:02:44 -0300

    add new images in consts

--- lib/app/consts/app_const.dart
+++ lib/app/consts/app_const.dart
@@ -1,5 +1,10 @@
 class AppConsts {
   static const String casseteImage = "assets/images/cassette-tape.jpg";
+  static const String guitar = "assets/images/guitar.jpg";
+  static const String electicGuitar = "assets/images/electric_guitar.jpg";
+  static const String headphone = "assets/images/headphone.jpg";
+  static const String people = "assets/images/people.jpg";
+  static const String piano = "assets/images/piano.jpg";
   static const String vinil =
       'assets/images/black-vinyl-record-playing-on-turntable-1389429.jpg';
 }
